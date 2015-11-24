790de4f597b1752254063d7f6306f274e44e6b51
Joshua McFarland <mcfarljw@gmail.com>
2015-11-24 12:55:16 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,129 @@
+var async = require('async');
+var escape = require('escape-string-regexp');
+var filesystem = require('fs');
+var logger = require('loggy');
+var lodash = require('lodash');
+
+
+/**
+ * @class ReplaceCompiler
+ * @param {Object} [config]
+ * @constructor
+ */
+function ReplaceCompiler(config) {
+    this.config = lodash.cloneDeep(config || {});
+    this.config = lodash.defaultsDeep(this.config, {
+        plugins: {
+            replace: {}
+        }
+    });
+}
+
+/**
+ * @property brunchPlugin
+ * @type {Boolean}
+ */
+ReplaceCompiler.prototype.brunchPlugin = true;
+
+/**
+ * @method getConfig
+ * @returns {Object}
+ */
+ReplaceCompiler.prototype.getConfig = function() {
+    return lodash.defaultsDeep(this.config.plugins.replace, {
+        encoding: 'utf8',
+        log: true,
+        mappings: {
+            date: (new Date()).toISOString(),
+            timestamp:  Math.floor(Date.now() / 1000)
+        },
+        paths: [],
+        replacePrefix: '{!',
+        replaceSuffix: '!}'
+    });
+};
+
+/**
+ * @method getPaths
+ * @param {Array} files
+ * @param {Object} config
+ * @returns {Array}
+ */
+ReplaceCompiler.prototype.getPaths = function(files, config) {
+    return lodash.pluck(files, 'path').filter(function(path) {
+        return !config.paths.length || config.paths.indexOf(path) > -1;
+    });
+};
+
+/**
+ * @method replaceFile
+ * @param {String} path
+ * @param {Object} config
+ * @param {Function} [callback]
+ */
+ReplaceCompiler.prototype.replaceFile = function(path, config, callback) {
+    filesystem.readFile(path, config.encoding, function(error, data) {
+        if (error) {
+            if (typeof callback === 'function') {
+                callback(error, path);
+            }
+        } else {
+            for (var key in config.mappings) {
+                if (config.mappings.hasOwnProperty(key)) {
+                    var searchString = escape(config.replacePrefix + key.toString() + config.replaceSuffix);
+                    var searchExpression = new RegExp(searchString, 'g');
+                    var replaceString = config.mappings[key.toString()];
+                    data = data.replace(searchExpression, replaceString);
+                }
+            }
+            filesystem.writeFile(path, data, config.encoding, function(error) {
+                if (error) {
+                    if (typeof callback === 'function') {
+                        callback(error, path);
+                    }
+                } else {
+                    if (typeof callback === 'function') {
+                        callback(null, path, data, config);
+                    }
+                }
+            });
+        }
+    });
+};
+
+/**
+ * @method onCompile
+ * @param {Array} files
+ * @param {Function} [callback]
+ */
+ReplaceCompiler.prototype.onCompile = function(files, callback) {
+    var config = this.getConfig();
+    var paths = this.getPaths(files, config);
+    var started = Date.now();
+    async.each(
+        paths,
+        lodash.bind(function(path, callback) {
+            this.replaceFile(path, config, callback);
+        }, this),
+        function(error, path) {
+            if (config.log) {
+                if (error) {
+                    logger.error(error)
+                } else {
+                    logger.info(
+                        'replaced mappings in ' +
+                        paths.length +
+                        ' files, replaced in ' +
+                        (Date.now() - started) +
+                        'ms'
+                    );
+                }
+            }
+            if (typeof callback === 'function') {
+                callback(error, path);
+            }
+        }
+    );
+};
+
+module.exports = ReplaceCompiler;
---
+++ test.js
@@ -0,0 +1,99 @@
+var async = require('async');
+var expect = require('chai').expect;
+var filesystem = require('fs');
+
+describe('Plugin', function() {
+    var Plugin = require('./');
+
+    afterEach(function(done) {
+        async.parallel([
+            async.apply(filesystem.writeFile, 'test_files/date.txt', '{!date!}', 'utf8'),
+            async.apply(filesystem.writeFile, 'test_files/timestamp.txt', '{!timestamp!}', 'utf8'),
+            async.apply(filesystem.writeFile, 'test_files/version.txt', '{?version?}', 'utf8')
+        ], done);
+    });
+
+    it('should compile user specified paths', function() {
+        var plugin = new Plugin({
+            plugins: {
+                replace: {
+                    paths: ['test_files/version.txt']
+                }
+            }
+        });
+        var paths = plugin.getPaths([
+            {path: 'test_files/date.txt'},
+            {path: 'test_files/timestamp.txt'},
+            {path: 'test_files/version.txt'}
+        ], plugin.getConfig());
+        expect(paths[0]).to.equal('test_files/version.txt');
+    });
+
+    it('should set default configs and paths', function() {
+        var plugin = new Plugin();
+        var config = plugin.getConfig();
+        var paths = plugin.getPaths([
+            {path: 'test_files/date.txt'},
+            {path: 'test_files/timestamp.txt'},
+            {path: 'test_files/version.txt'}
+        ], config);
+        expect(config.encoding).to.equal('utf8');
+        expect(config.mappings.date).to.be.a('string');
+        expect(config.mappings.timestamp).to.be.a('number');
+        expect(config.replacePrefix).to.equal('{!');
+        expect(config.replaceSuffix).to.equal('!}');
+        expect(paths).to.have.length(3);
+    });
+
+    it('should replace default string mappings', function(done) {
+        var plugin = new Plugin();
+        var config = plugin.getConfig();
+        async.parallel([
+            function(callback) {
+                plugin.replaceFile('test_files/date.txt', config, function(error, path, data) {
+                    if (error) {
+                        callback(error);
+                    } else {
+                        var date = data.toString();
+                        expect(date).to.have.equal(config.mappings.date);
+                        callback();
+                    }
+                });
+            },
+            function(callback) {
+                plugin.replaceFile('test_files/timestamp.txt', config, function(error, path, data) {
+                    if (error) {
+                        callback(error);
+                    } else {
+                        var date = parseInt(data, 10);
+                        expect(date).to.equal(config.mappings.timestamp);
+                        callback();
+                    }
+                });
+            }
+        ], done);
+    });
+
+    it('should replace user string mappings', function(done) {
+        var plugin = new Plugin({
+            plugins: {
+                replace: {
+                    mappings: {'version': '0.0.1'},
+                    replacePrefix: '{?',
+                    replaceSuffix: '?}'
+                }
+            }
+        });
+        var config = plugin.getConfig();
+        plugin.replaceFile('test_files/version.txt', config, function(error, path, data) {
+            if (error) {
+                done(error);
+            } else {
+                var version = data.toString();
+                expect(version).to.equal('0.0.1');
+                done();
+            }
+        });
+    });
+
+});
