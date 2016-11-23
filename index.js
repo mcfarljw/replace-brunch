baa77da013d65d77ed1edcc76f7788de59cf03f5
Joshua McFarland <mcfarljw@gmail.com>
2016-11-23 15:44:45 +0800

    Update plugin and tests to use es6 and promises

--- index.js
+++ index.js
@@ -1,154 +1,126 @@
-var async = require('async');
-var escape = require('escape-string-regexp');
-var filesystem = require('fs');
-var logger = require('loggy');
-var lodash = require('lodash');
+'use strict';
 
+const escape = require('escape-string-regexp');
+const filesystem = require('fs');
+const lodash = require('lodash');
+const logger = require('loggy');
 
-/**
- * @class ReplaceCompiler
- * @param {Object} [config]
- * @constructor
- */
-function ReplaceCompiler(config) {
-    this.config = lodash.cloneDeep(config || {});
+class ReplaceCompiler {
+
+  constructor(config) {
     this.config = lodash.defaultsDeep(
-        this.config,
-        {
-            plugins: {
-                replace: {}
-            }
+      lodash.cloneDeep(config || {}),
+      {
+        plugins: {
+          replace: {}
         }
+      }
     );
+  }
+
+  getConfig() {
+    return lodash.defaultsDeep(
+      this.config.plugins.replace,
+      {
+        encoding: 'utf8',
+        log: true,
+        mappings: {
+          date: (new Date()).toISOString(),
+          timestamp:  Math.floor(Date.now() / 1000)
+        },
+        paths: [],
+        replacePrefix: '{!',
+        replaceSuffix: '!}'
+      }
+    );
+  }
+
+  getPaths(files, config) {
+    return lodash
+      .chain(files)
+      .map(
+        (file) => {
+          return (file.destinationPath || file.destPath || file.path).replace(/\\/g, '/');
+        }
+      )
+      .filter(
+        (path) => {
+          return !config.paths.length || config.paths.indexOf(path) > -1;
+        }
+      )
+      .value();
+  }
+
+  replaceFile(path, config) {
+    return new Promise(
+      (resolve, reject) => {
+        filesystem.readFile(
+          path,
+          config.encoding,
+          (error, data) => {
+
+            if (error) {
+              return reject(error);
+            }
+
+            lodash.forIn(
+              config.mappings,
+              (value, key) => {
+                const searchString = escape(config.replacePrefix + key.toString() + config.replaceSuffix);
+                const searchExpression = new RegExp(searchString, 'g');
+
+                data = data.replace(searchExpression, value);
+              }
+            );
+
+            filesystem.writeFile(
+              path,
+              data,
+              config.encoding,
+              (error) => {
+                if (error) {
+                  return reject(error);
+                }
+
+                return resolve(data);
+              }
+            );
+          }
+        );
+      }
+    );
+  }
+
+  onCompile(files, assets) {
+    const started = Date.now();
+    const config = this.getConfig();
+    const paths = this.getPaths(files.concat(assets), config);
+
+    return Promise
+      .all(
+        lodash.map(
+          paths,
+          (path) => {
+            return this.replaceFile(path, config);
+          }
+        )
+      )
+      .then(
+        () => {
+          logger.info(
+            'replaced mappings in ' +
+            paths.length +
+            ' files, replaced in ' +
+            (Date.now() - started) +
+            'ms'
+          );
+        }
+      )
+      .catch((error) => logger.error(error));
+  }
+
 }
 
-/**
- * @property brunchPlugin
- * @type {Boolean}
- */
 ReplaceCompiler.prototype.brunchPlugin = true;
-
-/**
- * @method getConfig
- * @returns {Object}
- */
-ReplaceCompiler.prototype.getConfig = function() {
-    return lodash.defaultsDeep(
-        this.config.plugins.replace,
-        {
-            encoding: 'utf8',
-            log: true,
-            mappings: {
-                date: (new Date()).toISOString(),
-                timestamp:  Math.floor(Date.now() / 1000)
-            },
-            paths: [],
-            replacePrefix: '{!',
-            replaceSuffix: '!}'
-        }
-    );
-};
-
-/**
- * @method getPaths
- * @param {Array} files
- * @param {Object} config
- * @returns {Array}
- */
-ReplaceCompiler.prototype.getPaths = function(files, config) {
-    return lodash
-        .chain(files)
-        .map(function(file) {
-            return (file.destinationPath || file.destPath || file.path).replace(/\\/g, '/');
-        })
-        .filter(function(path) {
-            return !config.paths.length || config.paths.indexOf(path) > -1;
-        })
-        .value();
-};
-
-/**
- * @method replaceFile
- * @param {String} path
- * @param {Object} config
- * @param {Function} [callback]
- */
-ReplaceCompiler.prototype.replaceFile = function(path, config, callback) {
-    filesystem.readFile(
-        path,
-        config.encoding,
-        function(error, data) {
-            if (error) {
-                if (typeof callback === 'function') {
-                    callback(error, path);
-                }
-            } else {
-                for (var key in config.mappings) {
-                    if (config.mappings.hasOwnProperty(key)) {
-                        var searchString = escape(config.replacePrefix + key.toString() + config.replaceSuffix);
-                        var searchExpression = new RegExp(searchString, 'g');
-                        var replaceString = config.mappings[key.toString()];
-                        data = data.replace(searchExpression, replaceString);
-                    }
-                }
-                filesystem.writeFile(
-                    path,
-                    data,
-                    config.encoding,
-                    function(error) {
-                        if (error) {
-                            if (typeof callback === 'function') {
-                                callback(error, path);
-                            }
-                        } else {
-                            if (typeof callback === 'function') {
-                                callback(null, path, data, config);
-                            }
-                        }
-                    }
-                );
-            }
-        }
-    );
-};
-
-/**
- * @method onCompile
- * @param {Array} files
- * @param {Array} assets
- * @param {Function} [callback]
- */
-ReplaceCompiler.prototype.onCompile = function(files, assets, callback) {
-    var config = this.getConfig();
-    var paths = this.getPaths(files.concat(assets), config);
-    var started = Date.now();
-    async.each(
-        paths,
-        lodash.bind(
-            function(path, callback) {
-                this.replaceFile(path, config, callback);
-            },
-            this
-        ),
-        function(error, path) {
-            if (config.log) {
-                if (error) {
-                    logger.error(error)
-                } else {
-                    logger.info(
-                        'replaced mappings in ' +
-                        paths.length +
-                        ' files, replaced in ' +
-                        (Date.now() - started) +
-                        'ms'
-                    );
-                }
-            }
-            if (typeof callback === 'function') {
-                callback(error, path);
-            }
-        }
-    );
-};
 
 module.exports = ReplaceCompiler;
